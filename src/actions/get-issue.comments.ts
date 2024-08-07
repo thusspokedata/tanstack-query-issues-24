5938d371bd54ef9c5e710474502f24ed460e17e6
thusspokedata <thusspokedata@protonmail.com>
2024-08-07 02:26:27 +0200

    section4: Cargar comentarios del Issue

---
+++ src/actions/get-issue.comments.ts
@@ -0,0 +1,12 @@
+import { githubApi } from "../api/github.api";
+import { sleep } from "../helpers/sleep";
+import { GithubIssue } from "../issues/interfaces/issue.interface";
+
+
+export const getIssueComments = async(issueNumber: number):Promise<GithubIssue[]> => {
+    await sleep(1);
+  
+    const {data } = await githubApi.get<GithubIssue[]>(`/issues/${issueNumber}/comments`);
+
+    return data;
+}
--- src/hooks/useIssue.tsx
+++ src/hooks/useIssue.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getIssue } from "../actions/get-issue.action";
+import { getIssueComments } from "../actions/get-issue.comments";
 
 
 export const useIssue = (issueNumber: number) => {
@@ -8,9 +9,22 @@ export const useIssue = (issueNumber: number) => {
         queryKey: ['issues', issueNumber],
         queryFn: () => getIssue(issueNumber),
         staleTime: 1000 * 60, // 1 minute
-        retry: false
+    });
+
+    // const commentsQuery = useQuery({
+    //     queryKey: ['issues', issueNumber, "comments"],
+    //     queryFn: () => getIssueComments(issueNumber),
+    //     staleTime: 1000 * 60, // 1 minute
+    // });
+
+    const commentsQuery = useQuery({
+        queryKey: ['issues', issueQuery.data?.number, "comments"],
+        queryFn: () => getIssueComments(issueQuery.data!.number),
+        staleTime: 1000 * 60, // 1 minute
+        enabled: issueQuery.data !== undefined,
+        // enabled: !!issueQuery.data (es lo mismo que la linea de arriba),
     });
 
     
-    return {issueQuery}
+    return {issueQuery, commentsQuery}
 };
